5c4119ab564ff146d5580338c05767e5a6298a9b
Noga25 <57655759+Noga25@users.noreply.github.com>
2024-05-19 22:32:35 +0300

    Update MyRenderer.cs

--- Game Engine/ConsoleRendere/MyRenderer.cs
+++ Game Engine/ConsoleRendere/MyRenderer.cs
@@ -6,7 +6,8 @@ namespace ConsoleRendere
 {
     class MyRenderer : Renderer
     {
-        private const ConsoleKey ShowBoardKey = ConsoleKey.A;
+        private const ConsoleKey ShowBoardKey_TwoColors = ConsoleKey.A;
+        private const ConsoleKey ShowBoardKey_OneColors = ConsoleKey.B;
 
         public void Render(Renderer renderer)
         {
@@ -16,7 +17,9 @@ namespace ConsoleRendere
         public void RenderScreen()
         {
             Console.Clear();
-            Console.WriteLine("Welcome to the tile map engine rendere, Press A to see the board!");
+            Console.WriteLine("Welcome to the tile map engine rendere !!");
+            Console.WriteLine("Press A to see the board with two colors");
+            Console.WriteLine("Press B to see the board with one color");
             Console.WriteLine("Press any key to start...");
             Console.ReadKey(true);
             ShowBoard();
@@ -26,10 +29,16 @@ namespace ConsoleRendere
         {
             ConsoleKey keyInfo = Console.ReadKey().Key;
 
-            if (keyInfo == ShowBoardKey)
+            if (keyInfo == ShowBoardKey_TwoColors)
             {
                 Console.Clear();
-                Console.WriteLine("Pressed");
+                Console.WriteLine("Pressed A");
+            }
+
+            else if (keyInfo == ShowBoardKey_OneColors)
+            {
+                Console.Clear();
+                Console.WriteLine("Pressed B");
             }
         }
 
