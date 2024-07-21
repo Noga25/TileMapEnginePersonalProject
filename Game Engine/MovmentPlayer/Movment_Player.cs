b16cefc7ea47255593fba11865a56cae28c8fabb
Noga25 <57655759+Noga25@users.noreply.github.com>
2024-07-21 19:32:37 +0300

    Add movement assembly

---
+++ Game Engine/MovmentPlayer/Movment_Player.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovmentPlayer
+{
+    internal struct Movment_Player
+    {
+        private const string Player = "0";
+
+        private int x = 0;
+        private int y = 0;
+
+        public Movment_Player(int startX, int startY)
+        {
+            x = startX;
+            y = startY;
+            Console.Clear(); 
+            Write(Player, x, y);
+        }
+
+        public void MovePlayer()
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var command = Console.ReadKey(true).Key;
+
+                    switch (command)
+                    {
+                        case ConsoleKey.Z:
+                            Move(0, 1);
+                            break;
+
+                        case ConsoleKey.X:
+                            Move(0, -1);
+                            break;
+
+                        case ConsoleKey.C:
+                            Move(-1, 0);
+                            break;
+
+                        case ConsoleKey.V:
+                            Move(1, 0);
+                            break;
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(100);
+                }
+            }
+        }
+
+        private void Move(int deltaX, int deltaY)
+        {
+            // current position
+            Console.SetCursorPosition(x, y);
+            Console.Write(" ");
+
+            // Update position
+            x += deltaX;
+            y += deltaY;
+
+            // make sure position is within the bounds
+            x = Math.Max(0, x);
+            y = Math.Max(0, y);
+
+            // show the player at the new position
+            Write(Player, x, y);
+        }
+
+        private void Write(string toWrite, int x1, int y1)
+        {
+            try
+            {
+                Console.SetCursorPosition(x1, y1);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(toWrite);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Out of board boundaries");
+            }
+        }
+    }
+}
---
+++ Game Engine/MovmentPlayer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MovmentPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var player = new Movment_Player(0, 0);
+            player.MovePlayer();
+        }
+    }
+}
